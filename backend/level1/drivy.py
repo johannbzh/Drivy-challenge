680f701edc3053b6bd08184ed0347ad96092f5f7
Mathias Vast <mathias.vast@etu.utc.fr>
2020-06-04 22:13:02 +0200

    Replace drivy.py

--- backend/level1/drivy.py
+++ backend/level1/drivy.py
@@ -3,21 +3,21 @@ import datetime
 
 with open("data/input.json","r") as file:
     data=json.load(file)
-    cars=data.get("cars")
-    rentals=data.get("rentals")
+    cars=data["cars"]
+    rentals=data["rentals"]
     prices=[]
     
-    for i in rentals:
-        car_id=i.get("car_id")
+    for rental in rentals: #For each rental
+        car_id=rental["car_id"]
         dico={}
-        for j in cars:
-            if(j.get("id")==car_id):
-                price=j.get("price_per_day")*((datetime.datetime.strptime(i.get("end_date"),'%Y-%m-%d')-datetime.datetime.strptime(i.get("start_date"),'%Y-%m-%d')).days+1)+j.get("price_per_km")*i.get("distance") 
-        dico["id"]=i.get("id")
-        dico["price"]=price
+        for car_model in cars: #We look for the information of the corresponding vehicle
+            if(car_model["id"]==car_id): #We calculate the price
+                rent_price=car_model["price_per_day"]*((datetime.datetime.strptime(rental["end_date"],'%Y-%m-%d')-datetime.datetime.strptime(rental["start_date"],'%Y-%m-%d')).days+1)+car_model["price_per_km"]*rental["distance"] 
+        dico["id"]=rental["id"]
+        dico["price"]=int(rent_price)
         prices.append(dico)
     
     with open("output.json", "w") as write_file:
         output={}
         output["rentals"]=prices
-        json.dump(output, write_file)
+        json.dump(output, write_file,indent=4)
